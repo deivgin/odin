cff3e93a7485643a5e4fb438c226aa5220a2ea1a
Deividas Gineitis <deividasgineitis123@gmail.com>
2024-06-03 19:32:20 +0300

    Add css support

--- apps/library/src/index.ts
+++ apps/library/src/index.ts
@@ -1,19 +1,20 @@
-import express from "express";
-import libraryRouter from "./routes/library";
-import path from "node:path";
-import { libraryController } from "./controller/library.controller";
+import express from 'express';
+import libraryRouter from './routes/library';
+import path from 'node:path';
+import { libraryController } from './controller/library.controller';
 
 const app = express();
 const port = 3000;
 
-app.set("views", path.join(__dirname, "views"));
-app.set("view engine", "pug");
+app.use(express.static(path.join(__dirname, 'public')));
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
 
-app.use("/api", libraryRouter);
+app.use('/api', libraryRouter);
 
-app.get("/", (req, res) => {
+app.get('/', (req, res) => {
   const items = libraryController.getItems();
-  res.render("index", { title: "Library App", items });
+  res.render('index', { title: 'Library App', items });
 });
 
 app.listen(port, () => {
