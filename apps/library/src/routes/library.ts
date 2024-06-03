bd0fbd81fd2b959db1d0bb61d918f9a2c1334091
Deividas Gineitis <deividasgineitis123@gmail.com>
2024-06-03 16:35:59 +0300

    Prettier and add items functionality

--- apps/library/src/routes/library.ts
+++ apps/library/src/routes/library.ts
@@ -1,24 +1,27 @@
-import express from "express";
-import { libraryController } from "../controller/library.controller";
+import express from 'express'
+import { libraryController } from '../controller/library.controller'
+import bodyParser from 'body-parser'
 
-const router = express.Router();
+const router = express.Router()
 
-router.get("/getItems", (req, res) => {
-  const items = libraryController.getItems();
-  res.status(200).json(items);
-});
+router.get('/getItems', (req, res) => {
+  const items = libraryController.getItems()
+  res.status(200).json(items)
+})
 
-router.post("/addItem", (req, res) => {
-  console.log(req.body);
+router.post(
+  '/addItem',
+  bodyParser.urlencoded({ extended: false }),
+  (req, res) => {
+    const createdItem = libraryController.addItem(req.body)
+    res.render('item', { item: createdItem })
+  }
+)
 
-  const createdItem = libraryController.addItem(req.body);
-  res.status(201).json(createdItem);
-});
+router.delete('/deleteItem', (req, res) => {
+  const itemId = req.body
+  libraryController.deleteItem(itemId)
+  res.status(204).send()
+})
 
-router.delete("/deleteItem", (req, res) => {
-  const itemId = req.body;
-  libraryController.deleteItem(itemId);
-  res.status(204).send();
-});
-
-export default router;
+export default router
